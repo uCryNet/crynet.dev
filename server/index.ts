ee82b8a1ce070517739ca2ba8bbf5e3674198b54
crynet <ucrynet@gmail.com>
2022-11-22 17:40:38 +0200

    feat: https

--- server/index.ts
+++ server/index.ts
@@ -5,20 +5,23 @@ import fileUpload from "express-fileupload"
 import 'dotenv/config'
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import * as https from "https"
+import * as fs from "fs"
 
 // Variables
 const router = require("./src/routes/index.routes.ts")
-
 import { STATIC_FOLDER_NAME } from "./src/config/config"
 
-// origin needed because front and back works on different ports (only local develop)
-const originConf = !!process.env.ORIGIN && { origin: process.env.ORIGIN }
 
 const app = express()
 app.use(
   cors({
-    ...originConf,
-    credentials: true, // access-control-allow-credentials: true
+    // origin needed because front and back works on different ports (only local develop)
+    origin: `http://localhost:8080`,
+    credentials: true, //access-control-allow-credentials: true
+    // TODO: fix this
+    // @ts-ignore
+    optionSuccessStatus: 200,
   }),
 )
 app.use(cookieParser())
@@ -27,13 +30,21 @@ app.use(express.json())
 app.use(express.static(STATIC_FOLDER_NAME))
 app.use(fileUpload({}))
 app.use(router) // http://localhost:5000/api/
+mongoose.set('useCreateIndex', true)
 
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/crynet.tech/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/crynet.tech/cert.pem'),
+};
 
 async function startApp() {
   try {
-    mongoose.set('strictQuery', true)
-    await mongoose.connect(process.env.DB as string)
-    app.listen(process.env.PORT, () => console.log('===== SERVER WORKS ====='))
+    await mongoose.connect(process.env.DB as string, {
+      useFindAndModify: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    https.createServer(options, app).listen(process.env.PORT);
   } catch (e) {
     console.log(e)
   }
